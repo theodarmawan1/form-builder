3c8708614037740be9911f4c12010c2a23ce66e5
theo delfi <theot@MSI>
2024-12-13 14:31:21 +0800

    last label

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -7,8 +7,9 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 max-w-full sm:max-w-[300px] break-words"
 )
+
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
